5a1bec939ab24ebd60f668d065cd195ffa0b460b
Srđan Đukić <srd@mega.co.nz>
2022-06-08 15:56:04 +1200

    Add firewall to objects created
    Add the GCP firewall created manually up until now to the list of
    objects brought up by Ansible.
    
    This is also needed as without it the tear-down blocks with the network
    complaining that there is still a child object.
    
    Issue(s): None

--- gcp_dns/ansible/files/bootstrap.sh
+++ gcp_dns/ansible/files/bootstrap.sh
@@ -4,7 +4,7 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt upgrade
-apt install -y ansible
+apt install -y ansible git
 
 # Tell server what type it is
 echo "[dns]
@@ -14,6 +14,7 @@ echo "[dns]
 # TODO Pass server the secret
 
 # TODO Get server to pull config from GitHub
+ansible-pull -C add-gcp-free-tier-dns-server -U https://github.com/srkiNZ84/dukic_playbook.git gcp_dns/ansible/playbook.yml 
 
 # TODO Get server to run base config
 
